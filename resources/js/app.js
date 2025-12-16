05f967be77c7fb537f713fb24bc637994811eb54
Brandoxo <brandoxo90@gmail.com>
2025-12-16 17:08:31 -0600

    ADD\ Import inertia in app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,8 +1,14 @@
 import "./bootstrap";
 import AOS from "aos";
 import "aos/dist/aos.css";
+import '../css/app.css';  
+import { createInertiaApp } from "@inertiajs/vue3";
+import { createApp } from "vue";
+import { h } from "vue";
+import Header from "./Components/Header.vue";
+import Footer from "./Components/Footer.vue";
 
-// Inicializar AOS directamente (SIN función initAOS)
+// Inicializar AOS directamente
 document.addEventListener("DOMContentLoaded", function () {
     AOS.init({
         duration: 800,
@@ -12,4 +18,17 @@ document.addEventListener("DOMContentLoaded", function () {
     });
 
     console.log("AOS inicializado correctamente");
+});
+
+createInertiaApp({
+  resolve: (name) => {
+    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+    return pages[`./Pages/${name}.vue`];
+  },
+  setup({ el, App, props }) {
+    const app = createApp({ render: () => h(App, props) });
+    app.component("Header", Header);
+    app.component("Footer", Footer);
+    app.mount(el);
+  },
 });
