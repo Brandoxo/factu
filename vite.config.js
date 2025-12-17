3c1b659892538b1bb94f131ccf6360f39872aaef
Brandoxo <brandoxo90@gmail.com>
2025-12-17 14:00:15 -0600

    UPDATE\ Adding configuratio to import components

--- vite.config.js
+++ vite.config.js
@@ -2,8 +2,15 @@ import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 import tailwindcss from '@tailwindcss/vite';
 import vue from '@vitejs/plugin-vue';
+import { fileURLToPath, URL } from 'node:url';
 
 export default defineConfig({
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('./resources/js', import.meta.url)),
+            '@Components': fileURLToPath(new URL('./resources/js/Components', import.meta.url)),
+        },
+    },
     plugins: [
         vue(),
         laravel({
