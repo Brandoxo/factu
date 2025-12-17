115032efce7804448cb53b71bc115ccd74ff1936
Brandoxo <brandoxo90@gmail.com>
2025-12-17 15:32:59 -0600

    UPDATE\ svg lupa added to search button

--- resources/js/Components/OptionHotel.vue
+++ resources/js/Components/OptionHotel.vue
@@ -26,6 +26,9 @@
 
                             <div class="items-end flex">
                                 <button type="submit" class="p-2 px-6 w-full bg-blue-600 text-white rounded-full hover:bg-blue-700 cursor-pointer transition-all ease-in-out mt-4 md:mt-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block mr-2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                    </svg>
                                     Buscar
                                 </button>
                             </div>
--- resources/js/Components/OptionRestaurant.vue
+++ resources/js/Components/OptionRestaurant.vue
@@ -26,12 +26,15 @@
 
                             <div class="text-white">
                                 <InputLabel for="date" value="Fecha del consumo" class="text-white font-extralight text-xl w-full" />
-                                <Input id="date" class="block mt-4 w-full md:w-56 border-white border-2 rounded-xl p-2 bg-white/10" type="date" name="date" required />
+                                <Input id="date" class="block mt-4 w-full md:w-46 border-white border-2 rounded-xl p-2 bg-white/10" type="date" name="date" required />
                                 <InputError class="mt-2" :message="errors['date']" />
                             </div>
 
                             <div class="items-end flex lg:mt-12">
-                                <button type="submit" class="p-2 px-6 w-full bg-blue-600 text-white rounded-full hover:bg-blue-700 cursor-pointer transition-all ease-in-out mt-4 md:mt-0">
+                                <button type="submit" class="p-2 px-6 w-full lg:w-34 bg-blue-600 text-white rounded-full hover:bg-blue-700 cursor-pointer transition-all ease-in-out mt-4 md:mt-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 inline-block mr-2">
+                                        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+                                    </svg>
                                     Buscar
                                 </button>
                             </div>
