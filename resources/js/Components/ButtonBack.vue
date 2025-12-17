43eedc30a8fddd46be6e664364afd85d45b69f3c
Brandoxo <brandoxo90@gmail.com>
2025-12-17 14:01:31 -0600

    ADD\ New components

---
+++ resources/js/Components/ButtonBack.vue
@@ -0,0 +1,39 @@
+<template>
+    <button class="button-back"><svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6 text-white" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M11 19l-7-7 7-7m8 14l-7-7 7-7" />
+        
+    </svg></button>
+</template>
+    <style>
+      .button-back {
+            animation: slideInBack 1s ease-in-out;
+        }
+    
+        @keyframes slideInBack {
+            0% {
+                opacity: 0;
+                transform: translateY(-100%);
+            }
+    
+            25% {
+                opacity: 0.5;
+            }
+    
+            50% {
+                opacity: 1;
+            }
+    
+            100% {
+                opacity: 1;
+                transform: translateY(0);
+            }
+    
+            50% {
+                opacity: 1;
+            }
+    
+            100% {
+                opacity: 1;
+            }
+        }
+    </style>
---
+++ resources/js/Components/InputLabel.vue
@@ -0,0 +1,20 @@
+<script setup>
+defineProps({
+    value: {
+        type: String, default: '',
+    },
+    example: {
+        type: String,
+        default: '',
+    },
+        },);
+</script>
+
+<template>
+    <label class="flex text-sm font-bold text-black gap-x-2">
+        <span v-if="value">{{ value }}</span>
+        <span v-else><slot /></span>
+        <span class="text-gray-500 text-xs italic" v-if="example">({{ example }})</span>  
+        <span v-else></span>  
+    </label>
+</template>
