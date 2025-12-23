61a2299f4f366761b760a183835f7b9ed8eca379
Brandon Bermejo <brandoxo90@gmail.com>
2025-12-23 03:50:37 -0600

    UPDATE\ html background color black

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,28 +1,29 @@
 import "./bootstrap";
 import AOS from "aos";
 import "aos/dist/aos.css";
-import '../css/app.css';  
+import "../css/app.css";
 import { createInertiaApp } from "@inertiajs/vue3";
 import { createApp } from "vue";
 import { h } from "vue";
 import Header from "./Components/Header.vue";
 import Footer from "./Components/Footer.vue";
+import "sweetalert2/dist/sweetalert2.min.css";
 
 // Inicializar AOS directamente
 document.addEventListener("DOMContentLoaded", function () {
-    AOS.init({
-        duration: 800,
-        easing: "ease-in-out",
-        once: true,
-        offset: 100,
-    });
+  AOS.init({
+    duration: 800,
+    easing: "ease-in-out",
+    once: true,
+    offset: 100,
+  });
 
-    console.log("AOS inicializado correctamente");
+  console.log("AOS inicializado correctamente");
 });
 
 createInertiaApp({
   resolve: (name) => {
-    const pages = import.meta.glob('./Pages/**/*.vue', { eager: true });
+    const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
     return pages[`./Pages/${name}.vue`];
   },
   setup({ el, App, props }) {
