b173e38048da94cbb567db363f7d1c253bffd5b4
Brandoxo <brandoxo90@gmail.com>
2025-12-29 16:48:28 -0600

    UPDATE\ hotelData const exported

---
+++ resources/utils/hotelData.js
@@ -0,0 +1,15 @@
+export const dataHotel = { 
+  Name: "HOTEL RONDA MINERVA",
+  Description: "Hotel de 3 estrellas ubicado en el centro de la ciudad.",
+  Address: {
+    Street: "Av. Adolfo López Mateos Sur",
+    ExteriorNumber: "265",
+    InteriorNumber: "",
+    Neighborhood: "Jardines del bosque",
+    ZipCode: "44520",
+    Locality: "",
+    Municipality: "Guadalajara",
+    State: "Jalisco",
+    Country: "México"
+  }
+};
