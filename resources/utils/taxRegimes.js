d850cb4df74301a840153db86f6dfe9c18b54d83
Brandoxo <brandoxo90@gmail.com>
2025-12-29 16:47:23 -0600

    ADD\ taxRegimes const exported

---
+++ resources/utils/taxRegimes.js
@@ -0,0 +1,129 @@
+const taxRegimes = [
+    {
+      "clave": "601",
+      "nombre": "Régimen General de Ley Personas Morales",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "602",
+      "nombre": "Régimen Simplificado de Confianza (RESICO) Personas Morales",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "603",
+      "nombre": "Personas Morales con Fines No Lucrativos",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "604",
+      "nombre": "Régimen de Pequeños Contribuyentes (Derogado, pero la clave puede persistir para CFDI viejos)",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "605",
+      "nombre": "Sueldos y Salarios e Ingresos Asimilados a Salarios",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "606",
+      "nombre": "Régimen de Arrendamiento",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "607",
+      "nombre": "Régimen de Enajenación o Adquisición de Bienes",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "608",
+      "nombre": "Régimen de los Demás Ingresos (ej. Herencias, legados, premios)",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "609",
+      "nombre": "Régimen de Consolidación (Derogado)",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "610",
+      "nombre": "Residentes en el Extranjero sin Establecimiento Permanente en México",
+      "aplica_para": ["Ambos"]
+    },
+    {
+      "clave": "611",
+      "nombre": "Ingresos por Dividendos (Socios y Accionistas)",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "612",
+      "nombre": "Personas Físicas con Actividades Empresariales y Profesionales",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "613",
+      "nombre": "Régimen Intermedio de las Personas Físicas con Actividades Empresariales (Derogado)",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "614",
+      "nombre": "Ingresos por Intereses",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "615",
+      "nombre": "Ingresos por Obtención de Premios",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "616",
+      "nombre": "Sin Obligaciones Fiscales",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "618",
+      "nombre": "Régimen Simplificado de Confianza (RESICO) Personas Físicas",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "619",
+      "nombre": "Ingresos por la obtención de préstamos",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "620",
+      "nombre": "Sociedades Cooperativas de Producción que optan por diferir sus ingresos",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "621",
+      "nombre": "Régimen de Incorporación Fiscal (RIF) (En transición/extinción)",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "622",
+      "nombre": "Actividades Agrícolas, Ganaderas, Silvícolas y Pesqueras (AGAPES) Personas Morales",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "623",
+      "nombre": "Régimen Opcional para Grupos de Sociedades",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "624",
+      "nombre": "Régimen de los Coordinados",
+      "aplica_para": ["Persona Moral"]
+    },
+    {
+      "clave": "625",
+      "nombre": "Régimen de las Actividades Empresariales con ingresos a través de Plataformas Tecnológicas",
+      "aplica_para": ["Persona Física"]
+    },
+    {
+      "clave": "626",
+      "nombre": "Régimen Simplificado de Confianza (RESICO) Personas Físicas (Continuación/Clave alterna)",
+      "aplica_para": ["Persona Física"]
+    }
+  ];
+
+    export default taxRegimes;
