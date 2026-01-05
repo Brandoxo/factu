660a2a8740d1c3d604784646238c9e3381a2e845
Brandon Bermejo <brandoxo90@gmail.com>
2026-01-04 21:10:46 -0600

    FIX\ Returns the int data type instead of string

--- resources/utils/helpers.js
+++ resources/utils/helpers.js
@@ -1,4 +1,6 @@
 export const getTotalRate = (dailyRates) => {
   if (!Array.isArray(dailyRates)) return 0;
-  return dailyRates.reduce((sum, item) => sum + (item.rate || 0), 0);
+  const total = dailyRates.reduce((sum, item) => sum + (item.rate || 0), 0);
+  // Ensure we return a number to avoid string concatenation downstream
+  return Number(total.toFixed(2));
 };
