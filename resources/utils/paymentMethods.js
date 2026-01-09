bf7388f4fac2c25ddc63b432bf2ed8962686e901
Brandoxo <brandoxo90@gmail.com>
2026-01-09 10:28:28 -0600

    ADD\ Payment Methods

---
+++ resources/utils/paymentMethods.js
@@ -0,0 +1,22 @@
+export const paymentMethods = [
+    { 
+        name: 'Efectivo',
+        value: '01'
+    },    
+    { 
+        name: 'Cheque nominativo',
+        value: '02'
+    },
+    { 
+        name: 'Transferencia electrónica de fondos',
+        value: '03'
+    },
+    { 
+        name: 'Tarjeta de crédito',
+        value: '04'
+    },
+    { 
+        name: 'Tarjeta de débito',
+        value: '28'
+    },
+]
