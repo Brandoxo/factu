27afebc60c540ade5b090fb72db147d70975bc46
Brandoxo <brandoxo90@gmail.com>
2026-01-09 10:29:10 -0600

    ADD\ 'usoCfdiOptions' values

---
+++ resources/utils/usoCfdi.js
@@ -0,0 +1,8 @@
+export const usoCfdOptions = [
+  { value: "G01", label: "Adquisición de mercancías" },
+  { value: "G02", label: "Devoluciones, descuentos o bonificaciones" },
+  { value: "G03", label: "Gastos en general" },
+  { value: "I01", label: "Construcciones" },
+  { value: "I02", label: "Mobilario y equipo de oficina por inversiones" },
+  { value: "P01", label: "Por definir" },
+];
