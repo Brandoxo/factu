3491e5c14223ff66776704632c6cbfe2968cfdfe
Brandoxo <brandoxo90@gmail.com>
2026-02-09 15:14:26 -0600

    BUILD\ 1514 090226

--- vite.config.js
+++ vite.config.js
@@ -20,7 +20,7 @@ export default defineConfig({
         tailwindcss(),
     ],
     build: {
-        minify: 'terser',
+        minify: 'esbuild',
         terserOptions: {
             compress: {
                 drop_console: true,
