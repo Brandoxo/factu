2ced545c8691d3592305d0ed69e2cecfd0a76efa
Brandoxo <brandoxo90@gmail.com>
2026-02-09 13:15:45 -0600

    UPDATE\ nullable data to Invoice table

--- database/migrations/2026_01_19_210116_create_invoices_table.php
+++ database/migrations/2026_01_19_210116_create_invoices_table.php
@@ -14,19 +14,20 @@ return new class extends Migration
         if (!Schema::hasTable('invoices')) {
         Schema::create('invoices', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('fiscal_entity_id')->constrained('fiscal_entities');
+            $table->string('folio')->unique();
+            $table->foreignId('fiscal_entity_id')->nullable()->constrained('fiscal_entities');
             $table->string('reservation_id');
             $table->decimal('order_id', 15, 2);
-            $table->string('facturama_id');
-            $table->string('cfdi_uuid')->unique();
+            $table->string('facturama_id')->nullable();
+            $table->string('cfdi_uuid')->nullable()->unique();
             $table->enum('status', ['draft', 'pending', 'active', 'cancelled']);
-            $table->string('payment_form');
-            $table->string('payment_method');
-            $table->string('use_cfdi');
+            $table->string('payment_form')->nullable();
+            $table->string('payment_method')->nullable();
+            $table->string('use_cfdi')->nullable();
             $table->decimal('subtotal', 15, 2);
             $table->decimal('total', 15, 2);
-            $table->string('pdf_path');
-            $table->string('xml_path');
+            $table->string('pdf_path')->nullable();
+            $table->string('xml_path')->nullable();
             $table->timestamps();
         });
         }
