bac9e90c33a5769993d1f18c3f27d219e7c0ef03
Brandoxo <brandoxo90@gmail.com>
2026-02-09 13:16:41 -0600

    UPDATE\ Send folio in null value to backend

--- resources/utils/cfdiData.js
+++ resources/utils/cfdiData.js
@@ -4,7 +4,7 @@ export const createCfdiData = (form, items) => {
     Currency: "MXN", //Moneda en la que se emite el comprobante
     ExpeditionPlace: "44520", //Código postal del lugar de expedición
     PaymentConditions: "", //Condiciones de pago, puede estar vacío si no aplica
-    Folio: "100", //Probablemente se deba generar dinámicamente en la base de datos
+    Folio: null,// Folio se genera automáticamente en el backend, no se envía desde frontend
     CfdiType: "I", //Tipo de comprobante, "I" para ingresos o "E" para egresos
     PaymentForm: form.paymentMethod, //Código que indica la forma de pago, 01 para "Efectivo", 02 para "Cheque", 03 para "Transferencia electrónica", y 04 para "Tarjeta de crédito", etc
     PaymentMethod: "PUE", //Método de pago, "PUE" para "Pago en una sola exhibición" o "PPD" para "Pago en parcialidades o diferido"
